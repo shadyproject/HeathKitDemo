e75c9f290e6e9b09d3a8e09117ac8ecb85b403fc
Shady <shady@shadyproject.net>
2014-07-01 19:10:28 -0700

    Change FirstViewController to DataEntryController

--- HealthKitDemo/DataEntryController.h
+++ HealthKitDemo/DataEntryController.h
@@ -8,7 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface FirstViewController : UIViewController
+@interface DataEntryController : UITableViewController
 
 
 @end
--- HealthKitDemo/DataEntryController.m
+++ HealthKitDemo/DataEntryController.m
@@ -6,14 +6,14 @@
 //  Copyright (c) 2014 shadyproject. All rights reserved.
 //
 
-#import "FirstViewController.h"
+#import "DataEntryController.h"
 
-@interface FirstViewController ()
+@interface DataEntryController ()
             
 
 @end
 
-@implementation FirstViewController
+@implementation DataEntryController
             
 - (void)viewDidLoad {
     [super viewDidLoad];
